0be3fb44eef0bca783a49a72c34670205dc45522
Anaïs LEBRUN <an.lebrun1998@gmail.com>
2021-07-20 10:36:02 +0200

    img

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,15 +5,20 @@ import React from 'react';
 
 // == Imports locaux
 import './styles.scss';
+import img1 from './';
+import img2 from './img2/Accueil_2.jpg';
+import img3 from './img2/Accueil_3.jpg';
 
 const Home = () => (
   <Home className="Home">
-    <img src="//" alt="Logo" id="logo" />
+    {/*<img src="//" alt="Logo" id="logo" />*/}
     <div className="img-home">
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      <img src="../../public/Accueil 1.jpg" alt="mer" />
-      <img src="../../public/Accueil 2.jpg" alt="hiver" />
-      <img src="../../public/Accueil 3.jpg" alt="falaise" />
+      <div>
+        <img src={img1} alt="mer" />
+        {/*<img src={img2} alt="hiver" />*/}
+        {/*<img src={img3} alt="falaise" />*/}
+      </div>
     </div>
   </Home>
 );
