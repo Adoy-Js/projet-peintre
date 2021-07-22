632bc0448d4243a824fb333cda995b30cf0fe2c0
Adoy-Js <trillot.maxime@outlook.fr>
2021-07-22 09:58:02 +0200

    modify db.js

--- app/db.js
+++ app/db.js
@@ -1,10 +1,18 @@
-const {Pool} = require("pg");
+const {Pool} = require('pg');
 
-const pool = new Pool({
-    connectionString: process.env.PG_URL,
-    ssl:{
-        rejectUnauthorized:false
-    },
+let pool = new Pool({
+    connectionString: process.env.PG_URL
 });
+if (process.env.NODE_ENV === "production") {
+    pool = new Pool({
+        connectionString: process.env.DATABASE_URL,
+        // on ajoute cette option pour que heroku ne rejette pas les connexions non https à la DB
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+}
+
+//avec un Pool, plus besoin de se connecter avant l'export, c'est géré directement sous le capot par le Pool lui-même
 
 module.exports = pool;
