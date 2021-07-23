3176f4986b82ea2f56d74bbd2755ca68e247ffa8
Anaïs LEBRUN <an.lebrun1998@gmail.com>
2021-07-23 05:58:53 +0200

    axios for components LoginForm and Home

--- Front/src/components/Home/index.js
+++ Front/src/components/Home/index.js
@@ -1,30 +1,40 @@
 // == Import npm
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
+import axios from 'axios';
+
 
 // == Import
-import img1 from './Accueil_1.jpg';
-import img2 from './Accueil_2.jpg';
-import img3 from './Accueil_3.jpg';
+// import img1 from './Accueil_1.jpg';
+// import img2 from './Accueil_2.jpg';
+// import img3 from './Accueil_3.jpg';
 
 import './styles.scss';
 
 // == Composant
-const Home = () => (
+class Home extends PureComponent {
+    state = {
+      images: []
+    }
+  
+    componentDidMount() {
+      axios.get(`http://localhost:5000/about`)
+        .then(res => {
+          const images = res.data;
+          this.setState({ images });
+        })
+    }
+
+  render() {
+    return(
   <div className="app">
     <Splide>
-      <SplideSlide className="sea">
-        <img src={img1} alt="mer" />
-      </SplideSlide>
-      <SplideSlide className="snow">
-        <img src={img2} alt="neige" />
-      </SplideSlide>
-      <SplideSlide className="cliff">
-        <img src={img3} alt="falaise" />
+      <SplideSlide className="image">
+      { this.state.images.map(image => <li>{image.id}</li>)}
       </SplideSlide>
     </Splide>
   </div>
-);
+)}};
 
 // == Export
 export default Home;
---
+++ Front/src/components/Portrait/index.js
@@ -0,0 +1,14 @@
+// == Import de la lib React
+import React from 'react';
+
+// == Import npm
+
+// == Imports locaux
+import './styles.scss';
+
+const Portrait = () => (
+  <div>
+  </div>
+);
+
+export default Portrait;
