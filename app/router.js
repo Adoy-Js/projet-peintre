156b14bb7824abdd7f57f9ffc28eeda34fc5ff3d
Adoy-Js <trillot.maxime@outlook.fr>
2021-07-23 10:40:40 +0200

    router admin done, we must test now

--- app/router.js
+++ app/router.js
@@ -42,10 +42,10 @@ router.patch('/admin/home/:id', adminController.isAdmin, artistController.update
 //Artwork
 
 router.get('/admin/artwork', adminController.isAdmin, artworkController.getAll);
-// router.get('/admin/artwork/:category', adminController.isAdmin, artworkController.getAllByCategory);
-// router.post('/admin/artwork/:category', adminController.isAdmin, artworkController.addArtwork);
-// router.delete('/admin/artwork/:category/:id', adminController.isAdmin, artworkController.deleteArtwork);
-// router.patch('/admin/artwork/:category/:id', adminController.isAdmin, artworkController.updateArtwork);
+router.get('/admin/artwork/:category', adminController.isAdmin, artworkController.getAllByCategory);
+router.post('/admin/artwork/:category', adminController.isAdmin, artworkController.addArtwork);
+router.delete('/admin/artwork/:category/:id', adminController.isAdmin, artworkController.deleteArtwork);
+router.patch('/admin/artwork/:category/:id', adminController.isAdmin, artworkController.updateArtwork);
 
 //News
 
