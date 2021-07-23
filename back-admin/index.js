9662c29730151f37c70b92efbf0a37b212b6e702
Adoy-Js <trillot.maxime@outlook.fr>
2021-07-23 14:19:33 +0200

    use cors

--- back-admin/index.js
+++ back-admin/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 
 const router = require("./app/router");
 
@@ -13,6 +14,8 @@ app.use(express.json());
 app.use(express.urlencoded({
   extended: true,
 }));
+
+app.use(cors());
 
 app.use(router);
 
