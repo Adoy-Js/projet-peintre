9558760f6b444c66a426e7d793ade53dd05d2c55
Adoy-Js <trillot.maxime@outlook.fr>
2021-07-25 20:47:02 +0200

    database url fixed

--- app/db.js
+++ app/db.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 let pool = new Pool({
-    connectionString: process.env.PG_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false
     }
