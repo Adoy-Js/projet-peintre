27e1f7ef827dd69cd4a465d78e906c5ff1ff85ae
Anaïs LEBRUN <an.lebrun1998@gmail.com>
2021-07-26 15:29:54 +0200

    bio

--- Front/src/components/Biography/index.js
+++ Front/src/components/Biography/index.js
@@ -1,34 +1,25 @@
 // == Import de la lib React
 import React from 'react';
-// Ajout de la librairie de validation des props
-import PropTypes from 'prop-types';
 
 // == Imports locaux
 import './styles.scss';
 
-const Biography = ({ descriptions, messages, footermessages }) => (
+const Biography = () => (
   <div className="Contact">
     <div className="contact">
       <p className="contact_title">Biographie</p>
       <div className='contact_div'>
         <div>
-          Salut, moi c'est Camille.
+          Coucou, moi c'est Camille.
         </div>
         <div className="contact_footer">
           Allez salut hein ! <br />
-          Camille Paul
+          signé l'artiste moderne
         </div>
       </div>
     </div>
   </div>
 
 );
-
-// Validation des props
-Biography.propTypes = {
-  messages: PropTypes.string.isRequired,
-  footermessages: PropTypes.string.isRequired,
-};
-
 
 export default Biography;
