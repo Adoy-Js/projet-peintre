1354b5e27da945722027e2467bfebe6784f72d9f
Anaïs LEBRUN <an.lebrun1998@gmail.com>
2021-07-28 03:09:53 +0200

    setting axios in home and css for component Contact

--- Front/src/components/Home/index.js
+++ Front/src/components/Home/index.js
@@ -1,36 +1,43 @@
 // == Import npm
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { Splide, SplideSlide } from '@splidejs/react-splide';
-
-// == Import
-import img1 from './Accueil_1.jpg';
-import img2 from './Accueil_2.jpg';
-import img3 from './Accueil_3.jpg';
+import axios from 'axios';
 
 import './styles.scss';
 
 // == Composant
-const Home = () => (
-  <div className="home">
-    <div className="welcome">
-      Mon nom est Camille PAUL
-      <br /> Je souhaite vous montrer qui je suis et vous partager mes voyages à travers mon art
-    </div>
-    <div className="slider">
-      <Splide>
-        <SplideSlide className="sea">
-          <img src={img1} alt="mer" className="sea_image" />
-        </SplideSlide>
-        <SplideSlide className="snow">
-          <img src={img2} alt="neige" />
-        </SplideSlide>
-        <SplideSlide className="cliff">
-          <img src={img3} alt="falaise" />
-        </SplideSlide>
-      </Splide>
-    </div>
-  </div>
-);
+class Home extends PureComponent {
+  state = {
+    images: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://projet-peintre.herokuapp.com/about`)
+      .then(res => {
+        const images = res.data;
+        this.setState({ images });
+      })
+  }
+
+  render() {
+    return (
+      <div className="home">
+        <div className="welcome">
+          Mon nom est Camille PAUL
+          <br /> Je souhaite vous montrer qui je suis et vous partager mes voyages à travers mon art
+        </div>
+        <div className="app">
+          <Splide className="image">
+            <SplideSlide className="slide">
+              {this.state.images.map(image => <img className="image" key={image.id_picture} src={image.image} className="image_slider"></img>)}
+            </SplideSlide>
+          </Splide>
+        </div>
+      </div>
+    )
+  }
+};
 
 // == Export
 export default Home;
+
