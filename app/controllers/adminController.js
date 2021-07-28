9c598d3b5039af5a45c404d168d2f3172abf0f2f
Adoy-Js <trillot.maxime@outlook.fr>
2021-07-28 12:07:14 +0200

    JWT SECRET in .env

--- app/controllers/adminController.js
+++ app/controllers/adminController.js
@@ -1,23 +1,23 @@
 const Artist = require("../models/artist");
 const bcrypt = require("bcrypt");
 const jwtMiddleware = require("../service/jwt");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const adminController = {
   login: async (req, res, next) => {
     console.log("je suis dans admincontroller");
     const password = req.body.password;
-    const mail = req.body.mail;
+    const email = req.body.email;
 
-    if (mail == null || password == null) {
+    if (email == null || password == null) {
       return res.status(400).json({ error: "missing parameters" });
     }
 
-    const artistFounded = await Artist.findByMail(mail);
+    const artistFounded = await Artist.findByMail(email);
     if (artistFounded) {
       console.log(artistFounded);
     }
-
+    
     const storedPassword = artistFounded.password;
 
     if (password === storedPassword) {
@@ -30,28 +30,25 @@ const adminController = {
       // res.redirect('/admin/artwork');
     } else {
       return res.status(400).json({ error: "bad parameters" });
-
-
     }
   },
 
   isAdmin: async (req, res, next) => {
-    console.log('je suis dans isAdmin');
+    console.log("je suis dans isAdmin");
     next();
-    // const authHeader = req.headers.authorization;
-    // if(authHeader){
-    //   const token = authHeader.split(' ')[1];
-    //   jwt.verify(token, jwtMiddleware.JWT_SIGN_SECRET, (err, user) => {
-    //           if (err) {
-    //               return res.sendStatus(403);
-    //           }else{
-    //           console.log('ça s est bien passé');
-    //           req.user = user;
-    //           next();
-    //           }
-              
-    //       });
-    //     }
+    const authHeader = req.headers.Authorization;
+    if (authHeader) {
+      const token = authHeader.split(" ")[1];
+      jwt.verify(token, jwtMiddleware.JWT_SIGN_SECRET, (err, user) => {
+        if (err) {
+          return res.sendStatus(403);
+        } else {
+          console.log("ça s est bien passé");
+          req.user = user;
+          next();
+        }
+      });
+    }
   },
 };
 
