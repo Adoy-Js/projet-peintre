585b0c267ab47f9c8c078d8648ad4b96f915b5ce
FloVir <flo.vir@outlook.fr>
2021-07-29 10:15:02 +0200

    pictures-display branch

--- Front/src/components/Portrait/index.js
+++ Front/src/components/Portrait/index.js
@@ -1,5 +1,7 @@
 // == Import de la lib React
-import React, { useState } from 'react';
+import React, { PureComponent } from 'react';
+import axios from 'axios';
+
 
 import imgPortrait1 from './img/Portrait_1.jpg'
 import imgPortrait2 from './img/Portrait_2.jpg'
@@ -8,28 +10,38 @@ import imgPortrait2 from './img/Portrait_2.jpg'
 // == Imports locaux
 import './styles.scss';
 
-const Portrait = () => (
+
+
+class Portrait extends PureComponent {
+  state = {
+    images: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://projet-peintre.herokuapp.com/artwork/portrait`)
+      .then(res => {
+        const images = res.data;
+        this.setState({ images });
+      })
+  }
+
+  render() {
+    return (
   <div>
     <p className="portrait_introPortrait">Pourquoi vous contenter d'un selfie quand le dessin existe ?</p>
     <div className="portrait">
       <div className="portrait_p1">
-        <img src={imgPortrait1} alt="portrait" className="portrait_1" />
+      {this.state.images.map(image => <img className="image" key={image.picture_id} src={image.image} className="image_portrait"></img>)}
         <span>
           Dessin au Crayon graphite <br />
           21 x 29,7 cm (A4) <br />
           2018
         </span>
-      </div>
-      <div className="portrait_p2">
-        <img src={imgPortrait2} alt="portrait" className="portrait_2" />
-        <span>
-          Dessin au Crayon graphite <br />
-          21 x 29,7 cm (A4) <br />
-          2018
-        </span>
-      </div>
+        </div>
     </div>
   </div>
-);
+  )
+}
+};
 
 export default Portrait;
