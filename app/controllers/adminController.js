0c3df5d0d6146ee3a17c859ae5fe470efda423cb
Adoy-Js <trillot.maxime@outlook.fr>
2021-07-29 10:29:28 +0200

    maj

--- app/controllers/adminController.js
+++ app/controllers/adminController.js
@@ -34,8 +34,6 @@ const adminController = {
   },
 
   isAdmin: async (req, res, next) => {
-    console.log("je suis dans isAdmin");
-    next();
     const authHeader = req.headers.Authorization;
     if (authHeader) {
       const token = authHeader.split(" ")[1];
