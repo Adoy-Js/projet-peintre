e0d42fdd8b3ac078c7793005097f274a446f16bc
Anaïs LEBRUN <an.lebrun1998@gmail.com>
2021-07-30 02:15:38 +0200

    alignment of the text that goes with the portrait

--- Front/src/components/Client/Portrait/index.js
+++ Front/src/components/Client/Portrait/index.js
@@ -31,16 +31,23 @@ class Portrait extends PureComponent {
     return (
       <div>
         <p className="portraits">Pourquoi vous contenter d'un selfie quand le dessin existe ?</p>
+
         <div className="portrait">
-
           {this.state.images.map(image => <img className="portrait_image" key={image.picture_id} src={image.image}></img>)}
+        </div>   
 
-          {this.state.descriptions.map(description => <span className="description_description" key={description.picture_id}>{description.description}</span>)}
-          </div>
-          
+        <div className="description">
+          {this.state.descriptions.map(description => <span className="description" key={description.picture_id}>{description.description}</span>)}
+        </div>
+
+        <div className="description">
           {this.state.formats.map(format => <span className="description_format" key={format.picture_id}>{format.format}</span>)}
-          
+        </div>   
+
+        <div className="description">
           {this.state.dates.map(date => <span className="description_date" key={date.picture_id}>{date.date}</span>)}
+        </div>
+          
       </div >
     )
   }
