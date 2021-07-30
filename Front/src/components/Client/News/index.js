d305fdae7d7c6cc6405bcb95e02248101267984b
Anaïs LEBRUN <an.lebrun1998@gmail.com>
2021-07-30 03:51:39 +0200

    component News

--- Front/src/components/Client/News/index.js
+++ Front/src/components/Client/News/index.js
@@ -1,15 +1,26 @@
-// == Import npm
+// == Import de la lib React
 import React from 'react';
 
-// == Import
+// == Imports locaux
 import './styles.scss';
 
-// == Composant
 const News = () => (
-  <div className="Mural_paintings">
-      <h1>Actualités</h1>
+  <div className="new">
+    <div className="new_title">
+      Actualité
+    </div>
+      
+      <div className="news">
+        <div className="news_div">
+          J'aimerais bien exposer mes oeuvres mais je préfères regarder les oiseaux assis sur ma falaise. j'aime bien, c'est beau les oiseaux.
+        </div>
+        <div className="news_footer">
+          Ce serait aussi pratique que je trouve comment descendre rip le confort d'un bon matelat.  <br />
+          signé l'artiste moderne
+        </div>
+      </div>
   </div>
+
 );
 
-// == Export
 export default News;
