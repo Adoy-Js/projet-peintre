e39a57919616366d7f9a1cd6acabde3f8dfc1fce
Anaïs LEBRUN <an.lebrun1998@gmail.com>
2021-07-30 03:53:55 +0200

    correction

--- Front/src/components/Client/News/index.js
+++ Front/src/components/Client/News/index.js
@@ -12,7 +12,7 @@ const News = () => (
       
       <div className="news">
         <div className="news_div">
-          J'aimerais bien exposer mes oeuvres mais je préfères regarder les oiseaux assis sur ma falaise. j'aime bien, c'est beau les oiseaux.
+          J'aimerais bien exposer mes oeuvres mais je préfère regarder les oiseaux assis sur ma falaise. j'aime bien, c'est beau les oiseaux.
         </div>
         <div className="news_footer">
           Ce serait aussi pratique que je trouve comment descendre rip le confort d'un bon matelat.  <br />
