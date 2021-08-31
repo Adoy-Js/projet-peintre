3493fd50bd517e06c418e80f57e4d8117bb0cf34
Anaïs LEBRUN <an.lebrun1998@gmail.com>
2021-08-31 17:34:08 +0200

    implementation of the post home route

--- Front/src/components/Admin/ArrayHome/ArrayHomeForm/index.js
+++ Front/src/components/Admin/ArrayHome/ArrayHomeForm/index.js
@@ -1,31 +1,53 @@
 // == Import de la lib React
+import axios from "axios";
 import React, { useState } from "react";
-import DatePicker from "react-datepicker";
 
 
 // == Imports locaux
 import './styles.scss';
-import "react-datepicker/dist/react-datepicker.css";
 
 const ArrayHomeForm = () => {
+
+  const url = "http://localhost:5000/admin/home"
+  const [data, setData] = useState({
+    name: "",
+    image: "",
+  })
+
+  function handleSubmit(e){
+    e.preventDefault();
+
+    axios.post(url,{
+      name: data.name,
+      image: data.image,
+    }).then(res => {
+      console.log(res.data)
+    })
+  }
+
+  function handle(e) {
+    const newdata = {...data}
+    newdata[e.target.id] = e.target.value
+    setData(newdata)
+    console.log(newdata)
+  }
+
   return (
     <div className="arrayHomeForm">
       <div className="arrayHomeForm_title">
         Formulaire accueil
       </div>
-      <form className="arrayHomeForm_form">
+      <form onSubmit={(e) => handleSubmit(e)} className="arrayHomeForm_form">
         <div className="arrayHomeForm_name">
           Nom de la photo d'accueil:
-          <input className="arrayHomeForm_name_input" type="text" />
+          <input onChange={(e) => handle(e)} id="name" value={data.name} className="arrayHomeForm_name_input" type="text" />
         </div>
-      </form>
-      <form className="arrayHomeForm_formThree">
         <div className="arrayHomeForm_file">
-          <input className="arrayHomeForm_file_input" type="file" id="image" name="image" accept="image/png, image/jpeg" />
+          <input onChange={(e) => handle(e)} id="image" value={data.image} className="arrayHomeForm_file_input" type="file" accept="image/png, image/jpeg" />
         </div>
-      </form>
 
-      <button className="arrayHomeForm_ok">Valider</button>
+        <button className="arrayHomeForm_ok">Valider</button>
+      </form>
     </div>
   );
 };
