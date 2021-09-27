f88245d17113580cc4339530f1fbb62fda51ecc0
Max <trillot.maxime@outlook.fr>
2021-09-27 10:09:31 +0200

    jwt and bcrypt done

--- app/controllers/adminController.js
+++ app/controllers/adminController.js
@@ -1,56 +1,76 @@
 const Artist = require("../models/artist");
 const bcrypt = require("bcrypt");
-const jwtMiddleware = require("../service/jwt");
+const jwtService = require("../service/jwt");
 const jwt = require("jsonwebtoken");
 
 const adminController = {
+  addAdmin: async (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const salt = 10;
+
+    try {
+      bcrypt.hash(password, salt, async function (err, hash) {
+        await Artist.save(email, hash);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
   login: async (req, res, next) => {
     console.log("je suis dans admincontroller");
     const password = req.body.password;
     const email = req.body.email;
 
-    if (email == null || password == null) {
-      return res.status(400).json({ error: "missing parameters" });
-    }
+    try {
+      if (email == null || password == null) {
+        return res.status(400).json({ error: "missing parameters" });
+      }
 
-    const artistFounded = await Artist.findByMail(email);
-    if (artistFounded) {
-      console.log(artistFounded);
-    }
-    
-    const storedPassword = artistFounded.password;
+      const artistFounded = await Artist.findByMail(email);
+      if (artistFounded) {
+        console.log(artistFounded);
+      }
 
-    if (password === storedPassword) {
-      const token = await jwtMiddleware.generateToken(artistFounded);
-      console.log(token);
-      res.json({
-        userId: artistFounded.id_artist,
-        token: token,
-      });
-      // res.redirect('/admin/artwork');
-    } else {
-      return res.status(400).json({ error: "bad parameters" });
+      if (email !== artistFounded.email) {
+        throw new Error("Email faux");
+      }
+
+      const compare = await bcrypt.compare(password, artistFounded.password);
+
+      if (compare) {
+        res.json({
+          userId: artistFounded.id_artist,
+          token: jwtService.generateToken(artistFounded),
+        });
+      } else {
+        throw new Error("Mot de passe faux");
+      }
+    } catch (error) {
+      console.log(error);
     }
   },
 
   isAdmin: async (req, res, next) => {
-    console.log("je suis dans isAdmin");
-    next();
-    const authHeader = req.headers.Authorization;
-    if (authHeader) {
-      const token = authHeader.split(" ")[1];
-      jwt.verify(token, jwtMiddleware.JWT_SIGN_SECRET, (err, user) => {
+    
+    const authHeader = req.headers["authorization"];
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      console.log("pas de token");
+      res.sendStatus(401);
+    } else {
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
         if (err) {
-          return res.sendStatus(403);
+          console.log("mauvais token");
+          return res.sendStatus(401);
         } else {
-          console.log("ça s est bien passé");
+          console.log("bon token");
           req.user = user;
           next();
         }
       });
-    }else{
-      console.log("pas de Header autorization");
-
     }
   },
 };
