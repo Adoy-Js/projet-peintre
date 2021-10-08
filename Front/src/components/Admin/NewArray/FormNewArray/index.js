d90e3da126e4041818ee8bb53f5cd7aa928930f9
Max <trillot.maxime@outlook.fr>
2021-10-08 16:36:57 +0200

    picture for new news is not required now

--- Front/src/components/Admin/NewArray/FormNewArray/index.js
+++ Front/src/components/Admin/NewArray/FormNewArray/index.js
@@ -88,7 +88,6 @@ const FormNewArray = () => {
             placeholder="https://firebasestorage..."
             type="url"
             name="image"
-            required
           />
         </div>
 
