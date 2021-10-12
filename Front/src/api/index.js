0cad9ac473b0deeb1b333f16c5fe4efbdf5070b6
Max <trillot.maxime@outlook.fr>
2021-10-12 12:13:56 +0200

    api url from heroku

--- Front/src/api/index.js
+++ Front/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: ' https://projet-peintre.herokuapp.com',
   });
 
   export default instance;
