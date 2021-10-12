0be948d9dc4ed329590852df7961560339a768db
Anaïs LEBRUN <an.lebrun1998@gmail.com>
2021-10-12 20:28:43 +0300

    test

--- Front/src/components/Client/News/index.js
+++ Front/src/components/Client/News/index.js
@@ -1,27 +1,40 @@
 // == Import de la lib React
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 // == Imports locaux
 import Footer from '../Footer'
 import './styles.scss';
 
-const News = () => (
-  <div className="new">
-    <h1>
-      Actualité
-    </h1>
-      
+const News = () => {
+
+  const [data, setData] = useState([])
+
+
+  axios.get(`https://projet-peintre.herokuapp.com/news`)
+    .then(res => {
+      const data = res.data;
+      setData({ data });
+    })
+
+  return (
+    <div className="new">
+      <h1>
+        Actualité
+      </h1>
+
       <div className="news">
         <div>
           Actualité à venir.
         </div>
         <div className="news_footer">
-         Camille 
+          Camille
         </div>
       </div>
       <Footer />
-  </div>
+    </div>
 
-);
+  )
+};
 
 export default News;
