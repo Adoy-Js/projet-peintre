c5bd8f6820cf8d4608a58f0f137473c6bb0cff30
Anaïs LEBRUN <an.lebrun1998@gmail.com>
2021-10-12 20:38:52 +0300

    test

--- Front/src/components/Client/Menu/index.js
+++ Front/src/components/Client/Menu/index.js
@@ -1,6 +1,5 @@
 // import NPM
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
 import PropTypes from "prop-types";
 // == Imports locaux
 import "./styles.scss";
@@ -19,56 +18,56 @@ const Menu = ({ isLogged, onDisconnect }) => {
 
   return (
     <nav className={`menu ${showLinks ? "show-nav" : "hide-nav"} `}>
-      <NavLink to="/" className="item_menu menu_logo">
+      <a to="/" className="item_menu menu_logo">
         <img className="" src="" alt="" />
-      </NavLink>
+      </a>
       <ul className="links_menu">
         <li className="item_menu slideInDown-2">
-          <NavLink to="/" className="link_menu">
+          <a to="/" className="link_menu">
             Accueil
-          </NavLink>
+          </a>
         </li>
         <li className="item_menu slideInDown-3">
-          <NavLink to="/artwork/portrait" className="link_menu">
+          <a to="/artwork/portrait" className="link_menu">
             Portrait
-          </NavLink>
+          </a>
         </li>
         <li className="item_menu slideInDown-4">
-          <NavLink to="/artwork/painting" className="link_menu">
+          <a to="/artwork/painting" className="link_menu">
             Peintures
-          </NavLink>
+          </a>
         </li>
         <li className="item_menu slideInDown-5">
-          <NavLink to="/artwork/mural-painting" className="link_menu">
+          <a to="/artwork/mural-painting" className="link_menu">
             Peintures murales
-          </NavLink>
+          </a>
         </li>
         <li className="item_menu slideInDown-6">
-          <NavLink to="/news" className="link_menu">
+          <a to="/news" className="link_menu">
             Actualités
-          </NavLink>
+          </a>
         </li>
         <li className="item_menu slideInDown-7">
-          <NavLink to="/about" className="link_menu">
+          <a to="/about" className="link_menu">
             Biographie
-          </NavLink>
+          </a>
         </li>
         <li className="item_menu slideInDown-8">
-          <NavLink to="/contact" className="link_menu">
+          <a to="/contact" className="link_menu">
             Contact
-          </NavLink>
+          </a>
         </li>
         {isLogged && (
           <>
             <li className="item_menu disconnect">
-              <NavLink to="/" className="link_menu" onClick={handleDisconnect}>
+              <a to="/" className="link_menu" onClick={handleDisconnect}>
                 Déconnexion
-              </NavLink>
+              </a>
             </li>
             <li className="item_menu disconnect">
-              <NavLink to="/admin/artwork" className="link_menu">
+              <a to="/admin/artwork" className="link_menu">
                 Admin
-              </NavLink>
+              </a>
             </li>
           </>
         )}
